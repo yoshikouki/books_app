462bfbd95665f44b70939ebc33ac4fc82945f828
yoshikouki <yoshikouki@gmail.com>
2020-07-20 15:45:54 +0900

    clean/ RuboCop によるコード修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,15 +12,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_060939) do
-
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.text "memo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "author"
-    t.string "picture"
+ActiveRecord::Schema.define(version: 20_200_720_060_939) do
+  create_table 'books', force: :cascade do |t|
+    t.string 'title'
+    t.text 'memo'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'author'
+    t.string 'picture'
   end
-
 end
