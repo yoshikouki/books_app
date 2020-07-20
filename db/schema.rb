83ad686caa973e445b1436a2e70e83e9333f16e6
yoshikouki <yoshikouki@gmail.com>
2020-07-20 15:12:58 +0900

    add/ Book モデルに Picture 属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_054159) do
+ActiveRecord::Schema.define(version: 2020_07_20_060939) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_20_054159) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
