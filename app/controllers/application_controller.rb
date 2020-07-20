7532fe56eb563459eff79b8317ec4c7a6d7be65d
yoshikouki <yoshikouki@gmail.com>
2020-07-20 16:57:45 +0900

    I18n の導入にあたって初期設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # ロケール振り分けを全てのアクションで実行
+  around_action :switch_locale
+
+  # paramsのロケールによる振り分け
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  # url_for関係メソッドでロケールを設定するよう上書き
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,10 @@ module BooksApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # I18n
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.{rb,yml}')]
+    config.i18n.available_locales = %i[en ja]
+    config.i18n.default_locale = :ja
   end
 end
