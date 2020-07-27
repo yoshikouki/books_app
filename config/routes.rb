c0ebba0ddd0ac9bcb6177dbe0fea6e0240b32e9c
yoshikouki <yoshikouki@gmail.com>
2020-07-27 12:58:09 +0900

    GitHub認証ログインのためのルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users,
+                       controllers: { registrations: 'users/registrations',
+                                      omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     get 'users/:id', to: 'users/registrations#show'
   end
